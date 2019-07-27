a7cb8c5af52130a63bb315c2aa673bb9ef05e9f1
IvanLuLyf <835498692@qq.com>
2019-07-28 04:42:58 +0800

    Initial Commit

---
+++ admin/controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BunnyPHP\Admin\Controller;
+
+
+use BunnyPHP\Controller;
+
+class IndexController extends Controller
+{
+    public function ac_index()
+    {
+        echo 'Hello Admin Index';
+    }
+}
